404b3472a7394a563b062f30317e58cd4c42bee1
Simon Klee <hello@simonklee.dk>
2024-09-11 08:42:07 +0200

    init

---
+++ cleanup-notifications.js
@@ -0,0 +1,69 @@
+// @ts-check
+module.exports = async ({ github, context }) => {
+  /**
+   * Get an ISO date string for a given number of days in the past
+   * @param {number} days - Number of days in the past
+   * @returns {string} - ISO formatted date string
+   */
+  function getDate(days) {
+    const msInDay = 24 * 60 * 60 * 1000;
+    const now = new Date().getTime();
+    return new Date(now - (days || 1) * msInDay).toISOString();
+  }
+
+  const isWorkflowDispatch = context.eventName === "workflow_dispatch";
+  const before = isWorkflowDispatch ? undefined : getDate(1);
+  const since = getDate(3);
+
+  console.log(`🧹 Cleaning up notifications`);
+  console.log(`📡 Event name: ${context.eventName}`);
+  console.log(`📅 Current date: ${new Date().toISOString()}`);
+  console.log(`📅 Since: ${since}`);
+  console.log(`📅 Before: ${before}`);
+
+  // Fetch notifications
+  const notifications = await github.paginate("GET /notifications", {
+    all: true,
+    before,
+    since,
+  });
+
+  // Process each notification
+  for (const notif of notifications) {
+    let done = false;
+    const { type, unread } = notif.subject;
+
+    // Mark as done if notification is already read
+    if (!unread) {
+      done = true;
+    }
+
+    // Skip notifications for certain types
+    if (["Discussion", "CheckSuite", "Release"].includes(type)) {
+      done = true;
+    } else if (["Issue", "PullRequest"].includes(type)) {
+      const details = await github.request(`GET ${notif.subject.url}`);
+
+      // Mark as done if the issue/PR is closed
+      if (details.data.state === "closed") {
+        done = true;
+      }
+    }
+
+    // Remove "api." and "repos/" from the notification URL
+    const url = (notif.subject.url || notif.url)
+      .replace("api.", "")
+      .replace("repos/", "");
+
+    // Log and delete or skip the notification
+    const action = done ? "❌" : "✅";
+    console.log(
+      `${action} [${notif.repository.full_name}] ${notif.subject.title}\n  - ${url}`,
+    );
+
+    // Mark as read if done
+    if (done) {
+      await github.request(`PATCH /notifications/threads/${notif.id}`);
+    }
+  }
+};
